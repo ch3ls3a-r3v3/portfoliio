abff38bc4a67a8ae8b2e873e5c1e677e9ae1c520
ch3ls3a-r3v3 <ch3ls3a.r3v3@gmail.com>
2025-03-10 17:47:58 -0500

    Fixed framer-motion plug index.js

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {motion} from 'framer-motion';// ✅ Correct
+import motion from 'framer-motion';// ✅ Correct
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
