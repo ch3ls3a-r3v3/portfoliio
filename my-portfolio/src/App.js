41a5a837ab5a62cf3085fc318a8caf6cb0809996
ch3ls3a-r3v3 <ch3ls3a.r3v3@gmail.com>
2025-03-11 01:19:55 -0500

    Imported global.css

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import global from "./styles/global.css";
 
 function App() {
   return (
