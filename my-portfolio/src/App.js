d91c58d5a0043d0bd56a1b29bacaa9ffc3776933
ch3ls3a-r3v3 <ch3ls3a.r3v3@gmail.com>
2025-03-10 21:31:39 -0500

    mod and rename index.css and index.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,27 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import { motion } from "framer-motion";
+import "./App.css";  // Make sure App.css exists in /src
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div className="app-container">
+      <motion.h1
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        Welcome to My Portfolio
+      </motion.h1>
+    </div>
   );
 }
 
 export default App;
+
